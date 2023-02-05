8886f93bd5f44edde956f832b113119712d38355
0thw0foxa0 <97839499+0thw0foxa0@users.noreply.github.com>
2023-02-06 00:31:00 +0700

    Add files via upload

---
+++ vue.js
@@ -0,0 +1,20 @@
+// Vue.createApp({
+//     data(){
+//         return{
+//             counter:0
+//         }
+//     }
+// }).mount('#app')
+
+Vue.createApp({
+    data(){
+        return{
+            counter1:0,
+            counter2:0,
+            counter3:0,
+            counter4:0
+
+        }
+    }
+}).mount('#app')
+
